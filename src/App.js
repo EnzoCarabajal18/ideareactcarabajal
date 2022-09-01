c1c7d62b65c298d8b6ddd89dddcbf40ab68a5d1d
EnzoCarabajal18 <105323155+EnzoCarabajal18@users.noreply.github.com>
2022-09-01 11:09:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import'./app/style.css';
+import './App.css';
+import NavBar from './component/NavBar.js';
+
+const App = () => (
+    <div>
+        <NavBar />
+    </div>
+)
+  
+export default App;
---
+++ src/component/NavBar.js
@@ -0,0 +1,26 @@
+
+const NavBar = () => {
+  return (
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">IÑAKI</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Contactos</a>
+        <a class="nav-link" href="#">Sucursales</a>
+        <a class="nav-link" href="#">galería</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+    </header>
+  )
+}
+
+export default NavBar
