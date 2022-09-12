7f95377e3a2a3c7f72ba9352708bb584d56f4d29
EnzoCarabajal18 <105323155+EnzoCarabajal18@users.noreply.github.com>
2022-09-12 02:01:37 -0300

    Delete src directory

--- src/component/ItemList.js
+++
@@ -1,9 +0,0 @@
- import ItemListContainer from "./ItemListContainer";
-
-const Itemlist = ({data = []}) => {
-    return(
-        data.map(film => <Item key={film.id} info={film} />) 
-    );
-}
-
-export default ItemListContainer;
